af987751708e256ddfe5c0ae2500c0932fbb65d4
boberHelicopter <146185803+scout2281337@users.noreply.github.com>
2025-07-10 18:24:05 +0300

    0.3
    HealthSystem

---
+++ Assets/Scripts/Systems/HealthDeadSystem.cs
@@ -0,0 +1,26 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Collections;
+
+[UpdateInGroup(typeof(LateSimulationSystemGroup))]
+partial struct HealthDeadSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        EntityCommandBuffer entityCommandBuffer = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
+
+        foreach ((RefRO<Health> health, Entity entity) in SystemAPI.Query<RefRO<Health>>().WithEntityAccess()) 
+        {
+            if (health.ValueRO.healthAmount <= 0) 
+            {
+                entityCommandBuffer.DestroyEntity(entity);
+
+            }
+        }
+
+
+        
+    }
+
+}
--- Assets/Scripts/Systems/ShootAttackSystem.cs
+++ Assets/Scripts/Systems/ShootAttackSystem.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using Unity.Burst;
 using Unity.Entities;
 
@@ -26,6 +27,11 @@ partial struct ShootAttackSystem : ISystem
                 continue;
             }
             shootAttack.ValueRW.timer = shootAttack.ValueRO.timerMax;
+            Debug.Log("Shooting");
+
+            RefRW<Health> targetHealth = SystemAPI.GetComponentRW<Health>(target.ValueRO.targetEntity);
+            int damageAmount = 1;
+            targetHealth.ValueRW.healthAmount -= damageAmount;
         } 
     }
 
