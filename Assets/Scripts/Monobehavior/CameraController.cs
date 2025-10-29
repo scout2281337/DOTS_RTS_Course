a34906cf9c0d66a4b24d8092c0c0a3d3869c120c
boberHelicopter <146185803+scout2281337@users.noreply.github.com>
2025-10-29 18:05:54 +0300

    0.2.3
    New input system встроена + создана первая мапа для игрока + cameraController полностью через новые инпуты

--- Assets/Scripts/Monobehavior/CameraController.cs
+++ Assets/Scripts/Monobehavior/CameraController.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
+    private GameControls gameControls;
+    
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+    private Vector2 scrollInput;
+
     public float cameraMovementSpeed;
     public float scrollMovementSpeed;
 
@@ -14,7 +21,104 @@ public class CameraController : MonoBehaviour
 
     public GameObject mainCamera;
 
-    // Update is called once per frame
+    private bool isRightMouseButtonPressed;
+    private bool isPressedLeftShift;
+    private void Awake()
+    {
+        gameControls = new GameControls();
+        Vector3 angles = mainCamera.transform.eulerAngles;
+        yaw = angles.y;
+        pitch = angles.x;
+    }
+
+
+    private void OnEnable()
+    {
+        gameControls.Player.Enable();
+
+        gameControls.Player.Movement.performed += OnMovePerformed;
+        gameControls.Player.Movement.canceled += OnMoveCanceled;
+
+        gameControls.Player.CameraRotation.performed += OnLookPerformed;
+        gameControls.Player.CameraRotation.canceled += OnLookCanceled;
+
+        gameControls.Player.ClickForRotation.performed += OnClickPerformed;
+        gameControls.Player.ClickForRotation.canceled += OnClickCanceled;
+
+        gameControls.Player.ScreenZoom.performed += OnScrollPerformed;
+        gameControls.Player.ScreenZoom.canceled += OnScrollCanceled;
+
+        gameControls.Player.SpeedBoost.performed += OnSpeedBoostPreformed;
+        gameControls.Player.SpeedBoost.canceled += OnSpeedBoostCanceled;
+
+    }
+
+    private void OnDisable()
+    {
+        gameControls.Player.Movement.performed -= OnMovePerformed;
+        gameControls.Player.Movement.canceled -= OnMoveCanceled;
+
+        gameControls.Player.CameraRotation.performed -= OnLookPerformed;
+        gameControls.Player.CameraRotation.canceled -= OnLookCanceled;
+
+        gameControls.Player.ClickForRotation.performed -= OnClickPerformed;
+        gameControls.Player.ClickForRotation.canceled -= OnClickCanceled;
+
+        gameControls.Player.ScreenZoom.performed -= OnScrollPerformed;
+        gameControls.Player.ScreenZoom.canceled -= OnScrollCanceled;
+
+        gameControls.Player.SpeedBoost.performed -= OnSpeedBoostPreformed;
+        gameControls.Player.SpeedBoost.canceled -= OnSpeedBoostCanceled;
+
+        gameControls.Player.Disable();
+    }
+    private void OnMovePerformed(InputAction.CallbackContext context) 
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
+    }
+    private void OnLookPerformed(InputAction.CallbackContext context)
+    {
+        mouseInput = context.ReadValue<Vector2>();
+    }
+    private void OnLookCanceled(InputAction.CallbackContext context)
+    {
+        mouseInput = Vector2.zero;
+    }
+    private void OnClickPerformed(InputAction.CallbackContext context)
+    {
+        isRightMouseButtonPressed = true;    
+    }
+    private void OnClickCanceled(InputAction.CallbackContext context)
+    {
+        isRightMouseButtonPressed= false;
+    }
+    private void OnScrollPerformed(InputAction.CallbackContext context) 
+    {
+        scrollInput = context.ReadValue<Vector2>();   
+    }
+    private void OnScrollCanceled(InputAction.CallbackContext context)
+    {
+        scrollInput = Vector2.zero ;
+    }
+
+    private void OnSpeedBoostPreformed(InputAction.CallbackContext context) 
+    {
+        isPressedLeftShift = true;
+    }
+    private void OnSpeedBoostCanceled(InputAction.CallbackContext context)
+    {
+        isPressedLeftShift = false;
+    }
+
+
+
+
+
+
     void Update()
     {
         MoveCamera();
@@ -24,29 +128,25 @@ public class CameraController : MonoBehaviour
     public void MoveCamera() 
     {
         float speedMyltiplier = 1;
+        
         if (Input.GetKey(KeyCode.LeftShift)) 
         {
             speedMyltiplier = 3;   
         }
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        //transform.position += new Vector3(horizontalInput * cameraMovementSpeed * speedMyltiplier, 0f, verticalInput * cameraMovementSpeed * speedMyltiplier) * Time.deltaTime;
+        
 
-        transform.position += ((transform.forward * verticalInput) + (transform.right * horizontalInput)) * speedMyltiplier * cameraMovementSpeed * Time.deltaTime;
+        transform.position += ((transform.forward * moveInput.y) + (transform.right * moveInput.x)) * speedMyltiplier * cameraMovementSpeed * Time.deltaTime;
 
-        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
-        transform.position += mainCamera.transform.forward * Time.deltaTime * scrollMovementSpeed * scrollInput;
+
+        transform.position += mainCamera.transform.forward * Time.deltaTime * scrollMovementSpeed * scrollInput.y;
     }
 
     public void RotateCamera() 
     {
-        if (Input.GetMouseButton(2) ) 
+        if (isRightMouseButtonPressed) 
         {
-            float mouseX = Input.GetAxis("Mouse X");
-            float mouseY = Input.GetAxis("Mouse Y");
-
-            yaw += mouseX * sensitivity;
-            pitch -= mouseY * sensitivity;
+            yaw += mouseInput.x * sensitivity;
+            pitch -= mouseInput.y * sensitivity;
             pitch = Mathf.Clamp(pitch, pitchMin, pitchMax);
 
             mainCamera.transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
@@ -55,5 +155,6 @@ public class CameraController : MonoBehaviour
             transform.rotation = Quaternion.Euler( 0f, cameraEuler.y, 0f); 
         }
     }
+    
     
 }
